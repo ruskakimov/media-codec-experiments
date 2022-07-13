0d7deb46f04ba7b96aacba7a435afadec47a6655
Rustem Kakimov <ruskakimov@gmail.com>
2022-07-13 23:29:07 +0600

    Digital clock view

--- app/src/main/java/com/kakimov/mediacodecexperiments/MainActivity.kt
+++ app/src/main/java/com/kakimov/mediacodecexperiments/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.kakimov.mediacodecexperiments
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.time.LocalTime
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var digitalClockView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        digitalClockView = findViewById(R.id.digital_clock)
     }
 
     fun currentTimeString(): String {
