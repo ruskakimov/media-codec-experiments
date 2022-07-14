343622c7b1ae9b2e4b179277915066af04fb3035
Rustem Kakimov <ruskakimov@gmail.com>
2022-07-14 17:10:07 +0600

    Show digital clock

--- app/src/main/java/com/kakimov/mediacodecexperiments/MainActivity.kt
+++ app/src/main/java/com/kakimov/mediacodecexperiments/MainActivity.kt
@@ -4,16 +4,31 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.time.LocalTime
+import java.util.Timer
+import kotlin.concurrent.fixedRateTimer
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var digitalClockView: TextView
+
+    lateinit var timer: Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         digitalClockView = findViewById(R.id.digital_clock)
+
+        timer = fixedRateTimer("clock", false, 0L, 1000L) {
+            this@MainActivity.runOnUiThread {
+                digitalClockView.text = currentTimeString()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
     }
 
     fun currentTimeString(): String {
